0a3d7f9793d9d1552d12fb10e4bdf862374f7c04
David Gustafson <git@radicalskeptic.net>
2012-06-08 09:33:50 -0700

    first commit

---
+++ backup_checker.rb
@@ -0,0 +1,9 @@
+#load parameters from ini file
+require 'inifile'
+
+ini_contents = IniFile.new('backup_checker.ini').to_h['global']
+
+
+class BackupChecker
+  # To change this template use File | Settings | File Templates.
+end
