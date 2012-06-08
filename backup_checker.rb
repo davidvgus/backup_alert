bea4882a7d9a270f619edd5fd9356552c1ae1072
David Gustafson <git@radicalskeptic.net>
2012-06-08 15:31:22 -0700

    Stubbing out the main file and adding some files to test against.

--- backup_checker.rb
+++ backup_checker.rb
@@ -1,9 +1,47 @@
-#load parameters from ini file
-require 'inifile'
 
-ini_contents = IniFile.new('backup_checker.ini').to_h['global']
+
+
 
 
 class BackupChecker
-  # To change this template use File | Settings | File Templates.
+  #load parameters from ini file
+  require 'inifile'
+  require 'date'
+
+  attr_reader :ini_contents
+
+  def initialize()
+    @ini_contents = IniFile.new('backup_checker.ini').to_h['global']
+    @backup_dir = @ini_contents['backup_directory']
+    @working_directory = Dir.new(".")
+    @current_time = Time.now
+    @alert_message = ""
+    @dir_entries = ''
+  end
+
+
+
+  def alert?
+
+  end
+
+  def do_alert
+    #Log what you did for the alert
+  end
+
+  def check
+    #get entries for backup dir
+    #Check to see that there is at least one entry from previous day
+    #alert if there is no entry from previous day
+    #log
+  end
+
+  def get_dir_entries
+    
+  end
+
+  def log_write
+    #
+  end
+
 end
---
+++ sample/README.txt
@@ -0,0 +1,7 @@
+A backup set is made up of one *.4BK file, one *.4BR and 0 or more *.4BS files.
+
+The 4BR files are backup resorce files for a particular backup.
+
+The 4BK file is the initial backkup data file.
+
+The 4BS file is a segment file for when a 4BK file overflows
