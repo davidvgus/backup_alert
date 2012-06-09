33c3215f1f1e1d7327889eb2d9c44b84cdbcefdd
David Gustafson <git@radicalskeptic.net>
2012-06-08 17:04:38 -0700

    still creating skeleton/stubs, added sample/readme with info about badkup files

--- backup_checker.rb
+++ backup_checker.rb
@@ -1,7 +1,24 @@
 
+class BackupSet
 
+  def initialize(size)
+    @size = size
+    @set_date = ''
+    @set = []
+  end
 
+  def self.create_set(file_names)
+    backup_sets = {}
+    
+  end
 
+  def is_complete?
+    # A set is complete if it has at least one:
+    #*.4BK
+    #*.4BR
+    #and is of the minimum kilobyte size
+  end
+end
 
 class BackupChecker
   #load parameters from ini file
@@ -12,32 +29,33 @@ class BackupChecker
 
   def initialize()
     @ini_contents = IniFile.new('backup_checker.ini').to_h['global']
+    @minimum_size_in_k = 1
     @backup_dir = @ini_contents['backup_directory']
     @working_directory = Dir.new(".")
     @current_time = Time.now
     @alert_message = ""
     @dir_entries = ''
+    @log_file = 'log.txt'
   end
 
-
-
   def alert?
+    #
+  end
 
+  def check
+    #log
   end
 
   def do_alert
     #Log what you did for the alert
   end
 
-  def check
-    #get entries for backup dir
-    #Check to see that there is at least one entry from previous day
-    #alert if there is no entry from previous day
-    #log
+  def get_dir_entries
+
   end
 
-  def get_dir_entries
-    
+  def initialize_set
+    #
   end
 
   def log_write
--- sample/README.txt
+++ sample/README.txt
@@ -1,7 +1,10 @@
 A backup set is made up of one *.4BK file, one *.4BR and 0 or more *.4BS files.
 
-The 4BR files are backup resorce files for a particular backup.
+The 4BR files are backup resource files for a particular backup.
 
-The 4BK file is the initial backkup data file.
+The 4BK file is the initial backup data file.
 
 The 4BS file is a segment file for when a 4BK file overflows
+
+The file name after the site prefix ends in a backup sequence number
+and an optional dash and a segment number.
