e7e13902c2ad060675ffc765417637515a14c7dd
David Gustafson <git@radicalskeptic.net>
2012-06-28 16:24:47 -0700

    I moved the gmailer functionlity into a class and then moved the class into backup_checker

--- backup_checker.rb
+++ backup_checker.rb
@@ -193,4 +193,32 @@ class BackupChecker
 
 end
 
+class Gmailer
+  require 'tlsmail'
+  require 'time'
+  require 'parseconfig'
+
+
+  def send_report(email_config, message)
+    email_config = ParseConfig.new(email_config).params
+    from = email_config['from_address']
+    to = email_config['to_address']
+    p = email_config['p']
+    content = <<EOF
+From: #{from}
+To: #{to}
+subject: Backup Alert
+Date: #{Time.now.rfc2822}
+
+    #{message}
+EOF
+    print 'content', content
+
+    Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+    Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', from, p, :login) do |smtp|
+      smtp.send_message(content, from, to)
+    end
+  end
+end
+
 
