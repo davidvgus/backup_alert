fb8f83af958f8f167a7da61e3018aa18aa129ad0
David Gustafson <git@radicalskeptic.net>
2012-07-04 08:51:50 -0700

    removed old call to send_report, replaced it with send_report(email_config, report).

--- backup.rb
+++ backup.rb
@@ -9,19 +9,20 @@ require_relative 'backup_checker'
 email_config = File.join(Dir.home, "authinfo.txt")
 
 b = BackupChecker.new()
+mailer = EmailAlerter.new()
 
 if b.catalog.empty?
   report = "The Backup Directory (#{File.absolute_path(b.backup_dir)}) is empty of valid backup files."
   puts report
 
-  mailer = EmailAlerter.new()
+
   mailer.send_report(email_config, report)
 else
   report = b.report
   puts report
 
   if b.alert?
-    mailer = EmailAlerter.new()
+
     mailer.send_report(email_config, report)
   end
 end
